f1cf85da4ae2403fb0b9d4ae19e18b80bf997b57
obaidullahnaeem <obaidullahnaeem55@gmail.com>
2026-09-01 10:01:00 +0500

    this problem was difficult and was done using the help of ai but intresting

--- first.py
+++ first.py
@@ -2,6 +2,9 @@
 ### methodoverload , overrride
 
 # 9. Palindrome Number
+from more_itertools import strip
+
+
 class Solution(object):
     @classmethod
     def isPalindrome(self, x):
@@ -13,3 +16,40 @@ class Solution(object):
         else: return False
 # Solution.isPalindrome(121)
 
+# a = "abba"
+# print(a[::-1])
+
+# 234. Palindrome Linked List
+
+
+# 13. Roman to Integer
+class Solution(object):
+    def romanToInt(self, s):
+        values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        total = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+# 1 stop one bofore loop 
+# 2 subtract the small val
+                total -= values[s[i]]
+            else:  ## later add the value or simply add
+                total += values[s[i]]
+
+        return total
+solution = Solution()
+answer = solution.romanToInt("MCMXCIV")
+print(answer)
+
+# s = "MCMXCIV"
+# print(list(s))
+# print(s)
